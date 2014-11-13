5c7ef0e425dcd911f9505f7e05a4a4b6fd8415c8
Anthony B. Coates <abcoates@contakt.org>
2014-11-13 08:55:21 +0000

    Have now completed 'ResultToParameterMapping' and its tests.

--- src/main/scala/org/contakt/data/lineage/ResultToParameterMapping.scala
+++ src/main/scala/org/contakt/data/lineage/ResultToParameterMapping.scala
@@ -6,9 +6,13 @@ import scala.concurrent.{ExecutionContext, Future}
 /**
  * Defines a mapping from a result map into a new parameter map.
  */
-class ResultToParameterMapping(val nameMap: Map[String, String], val defaultMapping: ResultToParameterDefault = MAP_NONE)(implicit executionContext: ExecutionContext) {
+class ResultToParameterMapping(val nameMap: Map[String, String], val defaultMapping: ResultToParameterDefault = DEFAULT_MAP_NONE)(implicit executionContext: ExecutionContext) {
 
-  // TODO: Applies the name mapping to a set of results in order to create a new set of parameters.
+  /**
+   * Applies the mapping to the given results to produce a set of parameters.
+   * @param results result set for mapping
+   * @return mapped parameters from result set
+   */
   def map(results: ResultMap): ParameterMap = {
     val parameters = new mutable.HashMap[String, Future[Any]]() // temporary map for parameters
 
@@ -17,8 +21,8 @@ class ResultToParameterMapping(val nameMap: Map[String, String], val defaultMapp
         parameters put (nameMap(key), results(key))
       } else {
         defaultMapping match {
-          case MAP_ALL => parameters put (key, results(key))
-          case MAP_NONE => // do mothing
+          case DEFAULT_MAP_ALL => parameters put (key, results(key))
+          case DEFAULT_MAP_NONE => // do mothing
         }
       }
     }
@@ -26,30 +30,74 @@ class ResultToParameterMapping(val nameMap: Map[String, String], val defaultMapp
     new ParameterMap(Map[String, Future[Any]]() ++ parameters)(executionContext) // return result parameter map
   }
 
-  // TODO: returns a result-to-parameter mapping with an extra name mapping.
+  /**
+   * Postfix operator which provides a shorthand for 'map', e.g. 'resultsMap :: resultToParameterMapping'
+   * @param results result set for mapping
+   * @return mapped parameters from result set
+   */
+  def ::(results: ResultMap): ParameterMap = map(results)
+
+  /**
+   * Adds a mapping from a result name to a parameter name.
+   * @param mapping result (from) and parameter (to) name pair
+   * @return new ResultToParameterMapping with the additional mapping
+   */
   def addMapping(mapping: (String, String)): ResultToParameterMapping = {
     new ResultToParameterMapping(nameMap + mapping, defaultMapping)
   }
 
-  // TODO: scaladoc
+  /**
+   * Postfix operator which provides a shorthand for 'addMapping', e.g. '("a","b") :: initialMapping'
+   * @param mapping result (from) and parameter (to) name pair
+   * @return new ResultToParameterMapping with the additional mapping
+   */
+  def ::(mapping: (String, String)): ResultToParameterMapping = addMapping(mapping)
+
+  /**
+   * Adds a mapping from a result name to an identical parameter name.
+   * @param name name of both the result and the matching parameter, i.e. a same-name mapping
+   * @return new ResultToParameterMapping with the additional mapping
+   */
+  def addResult(name: String): ResultToParameterMapping = addMapping((name, name))
+
+  /**
+   * Postfix operator which provides a shorthand for 'addResult', e.g. ' "a" :: initialMapping'
+   * @param name name of both the result and the matching parameter, i.e. a same-name mapping
+   * @return new ResultToParameterMapping with the additional mapping
+   */
+  def ::(name: String): ResultToParameterMapping = addResult(name)
+
+  /**
+   * Adds mappings from result names to a parameter names.
+   * @param mappings result to parameter name mappings
+   * @return new ResultToParameterMapping with the additional mappings
+   */
   def addMappings(mappings: Map[String, String]): ResultToParameterMapping = {
     new ResultToParameterMapping(nameMap ++ mappings, defaultMapping)
   }
 
-  // TODO: scaladoc
-  def ::(mapping: (String, String)): ResultToParameterMapping = addMapping(mapping)
-
-  // TODO: scaladoc
+  /**
+   * Postfix operator which provides a shorthand for 'addMappings', e.g. 'Map("a"->"b", "c"->"d") :: initialMapping'
+   * @param mappings result to parameter name mappings
+   * @return new ResultToParameterMapping with the additional mappings
+   */
   def ::(mappings: Map[String, String]): ResultToParameterMapping = addMappings(mappings)
 
-  // TODO: scaladoc
-  def ::(results: ResultMap): ParameterMap = map(results)
+  /**
+   * Adds mappings from result names to identical parameter names.
+   * @param names names of both results and the matching parameters, i.e. all same-name mappings
+   * @return new ResultToParameterMapping with the additional mappings
+   */
+  def addResults(names: List[String]): ResultToParameterMapping = addMappings((names map {name => (name,name)}).toMap[String,String])
+
+  /**
+   * Postfix operator which provides a shorthand for 'addResults', e.g. 'List("a","b") :: initialMapping'
+   * @param names names of both results and the matching parameters, i.e. all same-name mappings
+   * @return new ResultToParameterMapping with the additional mappings
+   */
+  def ::(names: List[String]): ResultToParameterMapping = addResults(names)
 
 }
-
-//class ResultToParameterMapping_MAP_ALL(implicit executionContext: ExecutionContext) extends ResultToParameterMapping(Map[String, String](), ALL)(executionContext)
-//
-//class ResultToParameterMapping_MAP_NONE(implicit executionContext: ExecutionContext) extends ResultToParameterMapping(Map[String, String](), NONE)(executionContext)
 
 /**
  * Enumerated case class used to define the default behaviour of a result-to-parameter mapping.
@@ -58,7 +106,7 @@ class ResultToParameterMapping(val nameMap: Map[String, String], val defaultMapp
 sealed case class ResultToParameterDefault(name: Symbol) {}
 
 /** Default to mapping all results to a parameter of the same name, unless there is an explicit mapping. */
-object MAP_ALL extends ResultToParameterDefault('ALL)
+object DEFAULT_MAP_ALL extends ResultToParameterDefault('ALL)
 
 /** Default to no mapping of results to parameters of the same name, unless there is an explicit mapping. */
-object MAP_NONE extends ResultToParameterDefault('NONE)
+object DEFAULT_MAP_NONE extends ResultToParameterDefault('NONE)
--- src/test/scala/org/contakt/data/lineage/ResultToParameterMappingTests.scala
+++ src/test/scala/org/contakt/data/lineage/ResultToParameterMappingTests.scala
@@ -13,10 +13,103 @@ class ResultToParameterMappingTests extends FlatSpec with Matchers {
   RESULTS.addResult('a, Future{ 1 })
   RESULTS.addResult('b, Future{ 2 })
   RESULTS.addResult('c, Future{ 3 })
+  RESULTS.addResult('d, Future{ 4 })
+  RESULTS.addResult('e, Future{ 5 })
 
   "A result-to-parameter mapping" should "be able to map all results to parameters" in {
-    val params: ParameterMap = RESULTS :: MAP_ALL
-    assert(params.size === RESULTS.size)
+    val params: ParameterMap = RESULTS :: DEFAULT_MAP_ALL
+    assert(params.size === RESULTS.size, s"wrong number of results: expected ${RESULTS.size}, found ${params.size}")
+  }
+
+  it should "be able to map no results to parameters" in {
+    val params: ParameterMap = RESULTS :: DEFAULT_MAP_NONE
+    assert(params.size === 0, s"expected no results, found ${params.size}")
+  }
+
+  it should "be possible to map a single result name to a different parameter name" in {
+    val params: ParameterMap = RESULTS :: ("a" -> "f") :: DEFAULT_MAP_ALL
+    assert(params.size === RESULTS.size, s"wrong number of results: expected ${RESULTS.size}, found ${params.size}")
+    assert(!params.isDefinedAt("a"), "mapping failed, found unexpected 'a' parameter")
+    assert(params.isDefinedAt("b"), "mapping failed, did not find expected 'b' parameter")
+    assert(params.isDefinedAt("c"), "mapping failed, did not find expected 'c' parameter")
+    assert(params.isDefinedAt("d"), "mapping failed, did not find expected 'd' parameter")
+    assert(params.isDefinedAt("e"), "mapping failed, did not find expected 'e' parameter")
+    assert(params.isDefinedAt("f"), "mapping failed, did not find expected 'f' parameter")
+  }
+
+  it should "be possible to map multiple result names to different parameter names" in {
+    val params: ParameterMap = RESULTS :: Map("a" -> "f", "b" -> "g") :: DEFAULT_MAP_ALL
+    assert(params.size === RESULTS.size, s"wrong number of results: expected ${RESULTS.size}, found ${params.size}")
+    assert(!params.isDefinedAt("a"), "mapping failed, found unexpected 'a' parameter")
+    assert(!params.isDefinedAt("b"), "mapping failed, found unexpected 'b' parameter")
+    assert(params.isDefinedAt("c"), "mapping failed, did not find expected 'c' parameter")
+    assert(params.isDefinedAt("d"), "mapping failed, did not find expected 'd' parameter")
+    assert(params.isDefinedAt("e"), "mapping failed, did not find expected 'e' parameter")
+    assert(params.isDefinedAt("f"), "mapping failed, did not find expected 'f' parameter")
+    assert(params.isDefinedAt("g"), "mapping failed, did not find expected 'g' parameter")
+  }
+
+  it should "be possible to map a single result name to the same parameter name" in {
+    val params: ParameterMap = RESULTS :: "a" :: DEFAULT_MAP_NONE
+    assert(params.size === 1, s"expected 1 results, found ${params.size}")
+    assert(params.isDefinedAt("a"), "mapping failed, did not find expected 'a' parameter")
+  }
+
+  it should "be possible to map result names to the same parameter names" in {
+    val params: ParameterMap = RESULTS :: List("a","b") :: DEFAULT_MAP_NONE
+    assert(params.size === 2, s"expected 2 results, found ${params.size}")
+    assert(params.isDefinedAt("a"), "mapping failed, did not find expected 'a' parameter")
+    assert(params.isDefinedAt("b"), "mapping failed, did not find expected 'b' parameter")
+  }
+
+  it should "be possible to compose single-name mappings" in {
+    val params: ParameterMap = RESULTS :: ("a" -> "f") :: ("b" -> "g") :: DEFAULT_MAP_ALL
+    assert(params.size === RESULTS.size, s"wrong number of results: expected ${RESULTS.size}, found ${params.size}")
+    assert(!params.isDefinedAt("a"), "mapping failed, found unexpected 'a' parameter")
+    assert(!params.isDefinedAt("b"), "mapping failed, found unexpected 'b' parameter")
+    assert(params.isDefinedAt("c"), "mapping failed, did not find expected 'c' parameter")
+    assert(params.isDefinedAt("d"), "mapping failed, did not find expected 'd' parameter")
+    assert(params.isDefinedAt("e"), "mapping failed, did not find expected 'e' parameter")
+    assert(params.isDefinedAt("f"), "mapping failed, did not find expected 'f' parameter")
+    assert(params.isDefinedAt("g"), "mapping failed, did not find expected 'g' parameter")
+  }
+
+  it should "be possible to compose multi-name mappings" in {
+    val params: ParameterMap = RESULTS :: Map("a" -> "f", "b" -> "g") :: Map("c" -> "h", "d" -> "j") :: DEFAULT_MAP_ALL
+    assert(params.size === RESULTS.size, s"wrong number of results: expected ${RESULTS.size}, found ${params.size}")
+    assert(!params.isDefinedAt("a"), "mapping failed, found unexpected 'a' parameter")
+    assert(!params.isDefinedAt("b"), "mapping failed, found unexpected 'b' parameter")
+    assert(!params.isDefinedAt("c"), "mapping failed, found unexpected 'c' parameter")
+    assert(!params.isDefinedAt("d"), "mapping failed, found unexpected 'd' parameter")
+    assert(params.isDefinedAt("e"), "mapping failed, did not find expected 'e' parameter")
+    assert(params.isDefinedAt("f"), "mapping failed, did not find expected 'f' parameter")
+    assert(params.isDefinedAt("g"), "mapping failed, did not find expected 'g' parameter")
+    assert(params.isDefinedAt("h"), "mapping failed, did not find expected 'h' parameter")
+    assert(params.isDefinedAt("j"), "mapping failed, did not find expected 'j' parameter")
+  }
+
+  it should "be possible to compose a single name and a multi-name mapping" in {
+    val params: ParameterMap = RESULTS :: ("a" -> "f") :: Map("b" -> "g", "c" -> "h") :: DEFAULT_MAP_ALL
+    assert(params.size === RESULTS.size, s"[1] wrong number of results: expected ${RESULTS.size}, found ${params.size}")
+    assert(!params.isDefinedAt("a"), "[1] mapping failed, found unexpected 'a' parameter")
+    assert(!params.isDefinedAt("b"), "[1] mapping failed, found unexpected 'b' parameter")
+    assert(!params.isDefinedAt("c"), "[1] mapping failed, found unexpected 'c' parameter")
+    assert(params.isDefinedAt("d"), "[1] mapping failed, did not find expected 'd' parameter")
+    assert(params.isDefinedAt("e"), "[1] mapping failed, did not find expected 'e' parameter")
+    assert(params.isDefinedAt("f"), "[1] mapping failed, did not find expected 'f' parameter")
+    assert(params.isDefinedAt("g"), "[1] mapping failed, did not find expected 'g' parameter")
+    assert(params.isDefinedAt("h"), "[1] mapping failed, did not find expected 'h' parameter")
+
+    val params2: ParameterMap = RESULTS :: Map("a" -> "f", "b" -> "g") :: ("c" -> "h") :: DEFAULT_MAP_ALL
+    assert(params2.size === RESULTS.size, s"[2] wrong number of results: expected ${RESULTS.size}, found ${params2.size}")
+    assert(!params2.isDefinedAt("a"), "[2] mapping failed, found unexpected 'a' parameter")
+    assert(!params2.isDefinedAt("b"), "[2] mapping failed, found unexpected 'b' parameter")
+    assert(!params2.isDefinedAt("c"), "[2] mapping failed, found unexpected 'c' parameter")
+    assert(params2.isDefinedAt("d"), "[2] mapping failed, did not find expected 'd' parameter")
+    assert(params2.isDefinedAt("e"), "[2] mapping failed, did not find expected 'e' parameter")
+    assert(params2.isDefinedAt("f"), "[2] mapping failed, did not find expected 'f' parameter")
+    assert(params2.isDefinedAt("g"), "[2] mapping failed, did not find expected 'g' parameter")
+    assert(params2.isDefinedAt("h"), "[2] mapping failed, did not find expected 'h' parameter")
   }
 
 }
